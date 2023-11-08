060a71922079533c017933957bf472c5fa2bd449
brahim1219 <ibrahim.bensaada@esprit.tn>
2023-11-09 00:03:40 +0100

    mout

---
+++ src/test/java/tn/esprit/spring/kaddem/DepartementServiceTest.java
@@ -0,0 +1,35 @@
+package tn.esprit.spring.kaddem;
+
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import tn.esprit.spring.kaddem.entities.Departement;
+import tn.esprit.spring.kaddem.entities.Universite;
+import tn.esprit.spring.kaddem.services.IDepartementService;
+
+import java.util.List;
+
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class DepartementServiceTest {
+    @Autowired
+    IDepartementService departementService;
+    @Test()
+    @Order(1)
+    public void addDepar(){
+        List<Departement> DepartementList=departementService.retrieveAllDepartements();
+
+        Departement universite=new Departement("rii");
+        Departement univ= departementService.addDepartement(universite);
+        List<Departement> universiteList1=departementService.retrieveAllDepartements();
+        Assertions.assertEquals(DepartementList.size()+1,universiteList1.size());
+    }
+
+    @Test()
+    @Order(2)
+    public void retirveDepar(){
+        List<Departement> departements=departementService.retrieveAllDepartements();
+        Assertions.assertEquals(1,departements.size());
+
+    }
+}
---
+++ src/test/java/tn/esprit/spring/kaddem/UniversiteServiceTest.java
@@ -0,0 +1,36 @@
+package tn.esprit.spring.kaddem;
+
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import tn.esprit.spring.kaddem.entities.Universite;
+import tn.esprit.spring.kaddem.services.DepartementServiceImpl;
+import tn.esprit.spring.kaddem.services.UniversiteServiceImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class UniversiteServiceTest {
+    @Autowired
+    UniversiteServiceImpl universiteService;
+    @Test()
+    @Order(1)
+    public void addUniv(){
+        List<Universite> universiteList=universiteService.retrieveAllUniversites();
+
+        Universite universite=new Universite("rii");
+       Universite univ= universiteService.addUniversite(universite);
+        List<Universite> universiteList1=universiteService.retrieveAllUniversites();
+        Assertions.assertEquals(universiteList.size()+1,universiteList1.size());
+
+    }
+
+    @Test()
+    @Order(2)
+    public void retirveUniv(){
+        Assertions.assertEquals(2,universiteService.retrieveAllUniversites().size());
+
+    }
+}
